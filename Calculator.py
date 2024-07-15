9b06c59b681bdc1220453939883b7c1f88de70af
Ragapiriya <r2001piriya@gmail.com>
2024-07-15 16:26:09 +0530

    History feature has been added

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,14 @@
 #TODO: Write the functions for arithmatic operations here
 #These functions should cover Task 2
 import math
+history_list = []
+def history(history_list):
+    if len(history_list) >0:
+        for i in history_list:
+            print(i)
+    else:
+        print("No past calculations to show")
+
 def operandsInput():
     try:
         first_operand = input("Enter first number: ")
@@ -12,7 +20,7 @@ def operandsInput():
             print("Done. Terminating")
             raise SystemExit
         else:
-            print(int(first_operand))
+            print(first_operand)
         first_operand= float(first_operand)
         
         second_operand = input("Enter second number: ")
@@ -24,7 +32,7 @@ def operandsInput():
             print("Done. Terminating")
             raise SystemExit
         else:
-            print(int(second_operand))
+            print(second_operand)
         second_operand = float(second_operand)
         
         return first_operand,second_operand
@@ -33,21 +41,35 @@ def operandsInput():
 
 
 def addition(first_operand,second_operand):
-    print(str(first_operand) +" + "+ str(second_operand) +" = "+ str(first_operand+second_operand))
+    addition_answer=str(first_operand) +" + "+ str(second_operand) +" = "+ str(first_operand+second_operand)
+    print(addition_answer)
+    history_list.append(addition_answer)
 def substraction(first_operand,second_operand):
-    print(str(first_operand) +" - "+ str(second_operand) +" = "+ str(first_operand-second_operand))
+    substraction_answer=str(first_operand) +" - "+ str(second_operand) +" = "+ str(first_operand-second_operand)
+    print(substraction_answer)
+    history_list.append(substraction_answer)
 def multiplication(first_operand,second_operand):
-    print(str(first_operand) +" * "+ str(second_operand) +" = "+ str(first_operand*second_operand))
-def divion(first_operand,second_operand):
+    multiplication_answer=str(first_operand) +" * "+ str(second_operand) +" = "+ str(first_operand*second_operand)
+    print(multiplication_answer)
+    history_list.append(multiplication_answer)
+def division(first_operand,second_operand):
     if second_operand != 0:
-        print(str(first_operand) +" / "+ str(second_operand) +" = "+ str(first_operand/second_operand))
+        division_answer1=str(first_operand) +" / "+ str(second_operand) +" = "+ str(first_operand/second_operand)
+        print(division_answer1)
+        history_list.append(division_answer1)
     else:
+        division_answer2 = str(first_operand)+" / "+str(second_operand)+" = None"
         print("float division by zero")
-        print(str(first_operand)+" / "+str(second_operand)+" = None")
+        print(division_answer2)
+        history_list.append(division_answer2)
 def power(first_operand,second_operand):
-    print(str(first_operand) +" ^ "+ str(second_operand) +" = "+ str(math.pow(first_operand,second_operand)))
+    power_answer=str(first_operand) +" ^ "+ str(second_operand) +" = "+ str(math.pow(first_operand,second_operand))
+    print(power_answer)
+    history_list.append(power_answer)
 def remainder(first_operand,second_operand):
-    print(str(first_operand) +" % "+ str(second_operand) +" = "+ str(math.remainder(first_operand,second_operand)))
+    remainder_answer=str(first_operand) +" % "+ str(second_operand) +" = "+ str(math.remainder(first_operand,second_operand))
+    print(remainder_answer)
+    history_list.append(remainder_answer)
 #-------------------------------------
 #TODO: Write the select_op(choice) function here
 #This function sould cover Task 1 (Section 2) and Task 3
@@ -64,7 +86,7 @@ def select_op(choice):
                 if choice == "*":
                     multiplication(first_operand,second_operand)
                 if choice == "/":
-                    divion(first_operand,second_operand)
+                    division(first_operand,second_operand)
                 if choice == "^":
                     power(first_operand,second_operand)
                 if choice == "%":
@@ -73,6 +95,8 @@ def select_op(choice):
        return -1
     elif choice == "$":
         print("Resetting...")
+    elif choice =="?":
+        history(history_list)
     else:
         print("Unrecognized operation")
 
@@ -92,7 +116,8 @@ while True:
   print("6.Remainder: % ")
   print("7.Terminate: # ")
   print("8.Reset    : $ ")
-  choice = input("Enter choice(+,-,*,/,^,%,#,$): ")
+  print("9.History  : ? ")
+  choice = input("Enter choice(+,-,*,/,^,%,#,$,?): ")
   print(choice)
   if select_op(choice) == -1:
     #program ends here
